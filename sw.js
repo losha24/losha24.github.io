dfb399b096b13954f361c3fab536b2cb89567fcb
losha24 <losha24@gmail.com>
2026-02-23 11:53:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
-self.addEventListener('install',event=>{
-event.waitUntil(caches.open('finance-v1').then(cache=>{
-return cache.addAll(['index.html','manifest.json','icon-192.png','icon-512.png']);
-}));
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('finance-v1').then(cache => {
+            return cache.addAll(['index.html','manifest.json','icon-192.png','icon-512.png']);
+        })
+    );
 });
-self.addEventListener('fetch',event=>{
-event.respondWith(caches.match(event.request).then(res=>res||fetch(event.request)));
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
 });
